aded46230864525a130d351c27385169f67d9cfc
CarlosEduSC <carloseduardodas.c@gmail.com>
2024-05-08 19:29:33 -0300

    .

--- BackEnd/src/utils/format.util.ts
+++
@@ -1,7 +0,0 @@
-export function getOnlyNumbers(value: string) {
-  if (!value) {
-    return "";
-  }
-
-  return value.replace(/\D/g, "");
-}
--- BackEnd/src/utils/jsonwebtoken.util.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const TOKEN = process.env.JWT_SECRET_TOKEN || "";
-
-export const generateToken = (payload: object) => jwt.sign(payload, TOKEN);
-
-export const verifyToken = (token: string) => jwt.verify(token, TOKEN);
